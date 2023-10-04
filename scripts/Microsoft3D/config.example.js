9f77faee129e8959c7226bb1f804e91bc9d9494e
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-10-04 10:45:05 +0100

    feat: Add support for emoji picker

---
+++ scripts/Microsoft3D/config.example.js
@@ -0,0 +1,6 @@
+const config = {
+	["fluentui-emoji-path"]: "", // The root directory of the Fluent UI Emoji repo (https://github.com/microsoft/fluentui-emoji)
+	["sorted-path"]: "./sorted"
+}
+
+module.exports = config
--- scripts/Microsoft3D/sort.js
+++ scripts/Microsoft3D/sort.js
@@ -1,44 +1,51 @@
 const fs = require('fs')
-const coverter = require("discord-emoji-converter")
+const converter = require("discord-emoji-converter")
+const config = require("./config.js")
 
-const oldCss = ["https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/activity.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/flags.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/food.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/nature.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/objects.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/people.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/spritesheets.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/symbols.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/travel.css",]
+const fluentEmojiPath = config["fluentui-emoji-path"] + "/assets"
+const sortedPath = config["sorted-path"]
+
+const oldCss = ["https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/spritesheets.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/activity.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/flags.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/food.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/nature.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/objects.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/people.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/spritesheets.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/symbols.css", "https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft/travel.css",]
 const skinTones = {
 	"Default": "", "Light": "tone1", "Medium-Light": "tone2", "Medium": "tone3", "Medium-Dark": "tone4", "Dark": "tone5"
 }
 const emojiMetaData = {}
 const emojiList = {}
+const skinToneEmojis = {}
 const discordEmojiData = {}
+let discordSpriteSheets = {}
 const groups = {}
 
 async function downloadCSS() {
-	await fs.mkdirSync('./sorted2/css');
-	await fs.mkdirSync('./sorted2/css/Old');
+	await fs.mkdirSync(`${sortedPath}/css`);
+	await fs.mkdirSync(`${sortedPath}/css/Old`);
 	return Promise.all(oldCss.map(async (css) => {
 		const cssData = await fetch(css)
 		const cssText = await cssData.text()
 
 		const category = css.split("/").pop().replace(".css", "")
 
-		if (!fs.existsSync('./sorted2/css/Old/')) {
-			await fs.mkdirSync('./sorted2/css/Old/');
+		if (!fs.existsSync(`${sortedPath}/css/Old/`)) {
+			await fs.mkdirSync(`${sortedPath}/css/Old/`);
 		}
 
-		await fs.writeFileSync(`./sorted2/css/Old/${category}.css`, cssText)
+		await fs.writeFileSync(`${sortedPath}/css/Old/${category}.css`, cssText)
 	}))
 }
 
 async function sortOldCSS() {
-	const oldCssFiles = fs.readdirSync('./sorted2/css/Old')
+	const oldCssFiles = fs.readdirSync(`${sortedPath}/css/Old`)
+	oldCssFiles.splice(oldCssFiles.indexOf("spritesheets.css"), 1)
 
 	oldCssFiles.forEach((cssFile) => {
 		const fileName = cssFile.replace(".css", "")
 		discordEmojiData[fileName] = {}
-		const css = fs.readFileSync('./sorted2/css/Old/' + cssFile, 'utf8')
+		const css = fs.readFileSync(`${sortedPath}/css/Old/${cssFile}`, 'utf8')
 		const blocks = css.split(/\* .* \*/).map(block => block.trim()).filter(Boolean)
 
 		blocks.forEach(block => {
 			const assetMatches = block.match(/\/assets\/([\w\d]+\.svg)/g)
-			const emojiMatches = block.match(/https:\/\/mwittrien\.github\.io\/BetterDiscordAddons\/Themes\/EmojiReplace\/emojis\/Microsoft\/(\w+)\/(\w+\.\w+)"\)/)
+			const emojiMatches = block.match(/https:\/\/raw\.githubusercontent\.com\/Lythium4848\/EmojiReplace\/master\/emojis\/Microsoft\/(\w+)\/(\w+\.\w+)"\)/)
 
 			if (assetMatches && emojiMatches && assetMatches.length > 0 && emojiMatches.length > 1) {
 				const emoji = emojiMatches[2].replace(".png", "")
@@ -49,13 +56,22 @@ async function sortOldCSS() {
 		})
 	});
 
-	fs.writeFileSync('./sorted2/discordEmojiData.json', JSON.stringify(discordEmojiData))
+	fs.writeFileSync(`${sortedPath}/discordEmojiData.json`, JSON.stringify(discordEmojiData))
+
+	const oldSpriteSheetsCSS = fs.readFileSync(`${sortedPath}/css/Old/spritesheets.css`, 'utf8')
+	let matches = oldSpriteSheetsCSS.match(/url\("\/assets\/([^"]+)"\)/g);
+
+	matches = matches.map((match) => {
+		return match.replace(/url\("(.*)"\)/, "$1");
+	});
+
+	discordSpriteSheets = matches
 }
 
 async function getMetaData() {
-	const folders = fs.readdirSync('./assets')
+	const folders = fs.readdirSync(`${fluentEmojiPath}`)
 	return Promise.all(folders.map(async (folder) => {
-		const fileContent = fs.readFileSync(`./assets/${folder}/metadata.json`, 'utf8')
+		const fileContent = fs.readFileSync(`${fluentEmojiPath}/${folder}/metadata.json`, 'utf8')
 		const metadata = JSON.parse(fileContent)
 
 		metadata.cldr = folder
@@ -66,7 +82,7 @@ async function getMetaData() {
 		} else if (metadata.glyph === "👁️‍🗨️") {
 			name = ":eye_in_speech_bubble:"
 		} else {
-			name = coverter.getShortcode(metadata.glyph)
+			name = converter.getShortcode(metadata.glyph)
 		}
 
 		const cldr = metadata.cldr.replace(/[“”:’()]/g, "").replace("*", "asterisk").replace("#", "hashtag")
@@ -80,8 +96,8 @@ async function getMetaData() {
 			cldr: cldr,
 			name: name,
 			hasSkinTones: hasSkinTones,
-			path: `./assets/${folder}`,
-			copyPath: `./sorted2/emojis/${group}`,
+			path: `${fluentEmojiPath}/${folder}`,
+			copyPath: `${sortedPath}/emojis/${group}`,
 			group: group
 		}
 
@@ -98,6 +114,7 @@ async function getMetaData() {
 				emojiList[skinToneName] = {
 					name: skinToneName, group: group, glyph: metadata.glyph
 				}
+				skinToneEmojis[skinToneName] = emojiList[skinToneName]
 			}
 		} else {
 			emojiList[name] = {
@@ -110,12 +127,12 @@ async function getMetaData() {
 		}
 
 
-		fs.writeFileSync(`./assets/${folder}/metadata.json`, JSON.stringify(metadata))
+		fs.writeFileSync(`${fluentEmojiPath}/${folder}/metadata.json`, JSON.stringify(metadata))
 	}))
 }
 
 async function copyEmojis() {
-	await fs.mkdirSync('./sorted2/emojis');
+	await fs.mkdirSync(`${sortedPath}/emojis`);
 	return Promise.all(Object.keys(emojiMetaData).map(async (emoji) => {
 		const emojiData = emojiMetaData[emoji]
 		const emojiName = emojiData.name
@@ -128,8 +145,8 @@ async function copyEmojis() {
 		const fileCLDR = `${emojiCldr}_3d`
 		const fileName = `${emojiName}`
 
-		if (!fs.existsSync(`./sorted2/emojis/${emojiGroup}`)) {
-			await fs.mkdirSync(`./sorted2/emojis/${emojiGroup}`)
+		if (!fs.existsSync(`${sortedPath}/emojis/${emojiGroup}`)) {
+			await fs.mkdirSync(`${sortedPath}/emojis/${emojiGroup}`)
 		}
 
 		if (hasSkinTones) {
@@ -138,35 +155,43 @@ async function copyEmojis() {
 
 				let skinTonePath = `${fullPath}/${tone}/3D/${fileCLDR}`
 				let copyToPath = `${emojiData.copyPath}/${fileName}`
+				let skinToneName
 
 				if (tone === "Default") {
 					skinTonePath = `${skinTonePath}_default.png`
 					copyToPath = `${copyToPath}.png`
+					skinToneName = `${emojiName}`
+
 				} else {
 					skinTonePath = `${skinTonePath}_${tone.toLowerCase()}.png`
 					copyToPath = `${copyToPath}_${skinTone}.png`
+					skinToneName = `${emojiName}_${skinTone}`
 				}
 
+				emojiList[skinToneName].finalPath = copyToPath
 				await fs.copyFileSync(skinTonePath, copyToPath, fs.constants.COPYFILE_EXCL)
 			}
 
 			return
 		}
 
-		await fs.copyFileSync(`${fullPath}/3D/${fileCLDR}.png`, `${emojiData.copyPath}/${fileName}.png`, fs.constants.COPYFILE_EXCL)
+		const copyToPath = `${emojiData.copyPath}/${fileName}.png`
+		emojiList[emojiName].finalPath = copyToPath
+		await fs.copyFileSync(`${fullPath}/3D/${fileCLDR}.png`, copyToPath, fs.constants.COPYFILE_EXCL)
 	}))
 }
 
 async function createBaseCSSFiles() {
-	await fs.mkdirSync('./sorted2/css/New');
+	await fs.mkdirSync(`${sortedPath}/css/New`);
 	let baseData = ``
+	baseData += `@import url("https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft3D/spritesheets.css");\n`
 
 	for (const group in groups) {
-		fs.writeFileSync(`./sorted2/css/New/${group}.css`, ``)
+		fs.writeFileSync(`${sortedPath}/css/New/${group}.css`, ``)
 		baseData += `@import url("https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft3D/${group}.css");\n`
 	}
 
-	fs.writeFileSync(`./sorted2/css/New/base.css`, baseData)
+	fs.writeFileSync(`${sortedPath}/css/New/base.css`, baseData)
 }
 
 async function createNewCSS() {
@@ -189,17 +214,76 @@ async function createNewCSS() {
 .emoji[src='${emojiData.assets}'], .emoji[style*='background-image: url("${emojiData.assets}")'] {
 	background: transparent !important;
 	content: url("https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/emojis/Microsoft3D/${emojiGroup}/${emojiData.name}.png") !important;
-}\n`
-			fs.appendFileSync(`./sorted2/css/New/${emojiGroup}.css`, cssData)
+}`
+			await fs.appendFileSync(`${sortedPath}/css/New/${emojiGroup}.css`, cssData)
 		}
 	}
+
+	discordSpriteSheets.forEach((spriteSheet) => {
+		const cssData = `
+.emojiSpriteImage-3ykvhZ[style*="${spriteSheet}"]{
+    background-image: url("") !important;
+}
+			`
+
+		fs.appendFileSync(`${sortedPath}/css/New/spritesheets.css`, cssData)
+	})
+}
+
+async function generateSpriteSheets(start) {
+	await fs.mkdirSync(`${sortedPath}/spritesheets`);
+
+	await fs.appendFileSync(`${sortedPath}/css/New/spritesheets.css`, `
+.emoji-4YP39J[src*="/assets/"] {
+    display: none;
+}`)
+
+
+	for (const key in emojiList) {
+		const emoji = emojiList[key]
+		let cssData = `
+/* ${emoji.name} */
+.emojiItem-277VFM[data-name="${emoji.name}"]:not([data-id])  {
+\tbackground-image: url("https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/emojis/Microsoft3D/${emoji.group}/${emoji.name}.png") !important;
+    background-size: contain !important;
+}
+		`
+
+		await fs.appendFileSync(`${sortedPath}/css/New/spritesheets.css`, cssData)
+	}
+
+
+	await fs.mkdirSync(`${sortedPath}/css/New/picker`)
+	for (const group in discordEmojiData) {
+		const groupData = discordEmojiData[group]
+		for (const emoji in groupData) {
+			const emojiData = groupData[emoji]
+			const emojiGroup = emojiList[emojiData.name]?.group
+			if (!emojiGroup) continue
+
+			let cssData = `
+.graphicPrimary-jNHB2G:has(.emoji-4YP39J[src="${emojiData.assets}"]) {
+    background-image: url("https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/emojis/Microsoft3D/${emojiGroup}/${emojiData.name}.png") !important;
+    background-size: contain !important;
+}`
+
+			await fs.appendFileSync(`${sortedPath}/css/New/picker/${emojiGroup}.css`, cssData)
+		}
+	}
+
+	let importData = ``
+	for (const group in groups) {
+		importData += `@import url("https://raw.githubusercontent.com/Lythium4848/EmojiReplace/master/src/Microsoft3D/picker/${group}.css");\n`
+	}
+
+	fs.appendFileSync(`${sortedPath}/css/New/spritesheets.css`, importData)
 }
 
 async function main() {
-	if (fs.existsSync('./sorted2')) {
-		await fs.rmSync('./sorted2', {recursive: true});
+	if (fs.existsSync(`${sortedPath}`)) {
+		await fs.rmSync(`${sortedPath}`, {recursive: true});
 	}
-	await fs.mkdirSync('./sorted2');
+	await fs.mkdirSync(`${sortedPath}`);
 
 	await downloadCSS()
 	console.log("Downloaded old CSS files.")
@@ -210,8 +294,8 @@ async function main() {
 	await getMetaData()
 	console.log("Fixed metadata.")
 
-	await fs.writeFileSync('./sorted2/emojiMetaData.json', JSON.stringify(emojiMetaData))
-	await fs.writeFileSync('./sorted2/emojiList.json', JSON.stringify(emojiList))
+	await fs.writeFileSync(`${sortedPath}/emojiMetaData.json`, JSON.stringify(emojiMetaData))
+	await fs.writeFileSync(`${sortedPath}/emojiList.json`, JSON.stringify(emojiList))
 
 	await createBaseCSSFiles()
 	console.log("Created base CSS files.")
@@ -221,6 +305,9 @@ async function main() {
 
 	await createNewCSS()
 	console.log("Created new CSS files.")
+
+	await generateSpriteSheets()
+	console.log("Generated spritesheets.")
 
 }
 
